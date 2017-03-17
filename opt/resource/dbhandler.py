25870f2d4a9fa0f583b8e56e6cd5792d5412b644
markuszm <markus_zimmermann-ffm@t-online.de>
2017-03-17 17:04:22 +0100

    Fixed fetching in DBHandler

--- opt/resource/dbhandler.py
+++ opt/resource/dbhandler.py
@@ -53,7 +53,7 @@ class DBHandler:
 
     def user_exists(self, username):
         self.execute_db_command("SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '{}');".format(username))
-        if self.cursor.fetchall() == (1,):
+        if self.cursor.fetchone() == (1,):
             return True
         else:
             return False
@@ -63,7 +63,7 @@ class DBHandler:
             SELECT count(*)
             FROM information_schema.TABLES
             WHERE (TABLE_SCHEMA = '{}') AND (TABLE_NAME = '{}');""".format(database, table))
-        if self.cursor.fetchall() == (1,):
+        if self.cursor.fetchone() == (1,):
             return True
         else:
             return False
